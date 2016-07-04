08e421c6d3ff1fceb2e9bfda613f0789b5241290
Vanqingyu <vanqingyu@sina.com>
2016-07-04 17:45:33 +0800

    Tojson 实现实时读数据

--- Enity.py
+++ Enity.py
@@ -3,7 +3,8 @@ import DBManage
 class Enity:
     sql = "SELECT [ID],[NAME] ,[ENTITY_TYPE],[PARENT_ID],[TYPE_ID]  FROM [VIM_VCDB].[dbo].[VPXV_ENTITY] order by id "
     enities=[]
-    def __init__(self):
+    def __init__(self,enities):
+        self.enities=enities
         db=DBManage.DBManage(host="192.168.1.200",port="1433",user="vm",password="password1!",database="VIM_VCDB",charset="UTF-8")
         db.getConnetion()
         self.cursor=db.QuerySql(self.sql)
--- ToJson.py
+++ ToJson.py
@@ -1,74 +1,104 @@
 #-*- coding:UTF-8 -*-
+import os
 import json
 import Enity
-enity= Enity.Enity()
-enities=enity.getEnity()
-i=1
-j=1
-source=[]
-target=[]
-id=[]
-imageSwitch={0:"res/image/vmware.png",
-             1:"res/image/host.png",
-             7:"res/image/disk.png",
-             8:"res/image/disk.png",
-             16:"res/image/disk.png",
-             18:"res/image/disk.png",
-             19:"res/image/host.png",
-             14:"res/image/host.png",
-             15:"res/image/host.png",
-             17:"res/image/host.png"}
+import time
+from threading import Thread
+import threading
 
-
-for row in enities:
-    source.append(row[0])
-    id.append(row[0])
-for row in enities:
-    if(row[3]==None):
-        target.append(0)
-    else:
-        target.append(row[3])
-
-for x in range(len(id)):
-    for y in range(len(target)):
-        if(id[x]==source[y]):
-            source[y]=x
-        if(id[x]==target[y]):
-            target[y]=x
-
-
-with open('graph.json', 'wt',encoding="UTF-8") as f:
-#nodes:
-    f.write("{\n\"nodes\":\n[\n")
-    for row in enities:
-
-        img = "res/image/vmware.png"
-        if row[4] in imageSwitch:
-            typeid=row[4]
-            img=imageSwitch[typeid]
+imageSwitch = {0: "res/image/vmware.png",
+               1: "res/image/host.png",
+               7: "res/image/disk.png",
+               8: "res/image/disk.png",
+               16: "res/image/disk.png",
+               18: "res/image/disk.png",
+               19: "res/image/host.png",
+               14: "res/image/host.png",
+               15: "res/image/host.png",
+               17: "res/image/host.png"}
 
 
 
-        id=row[0]
-        name=row[1]
-        f.write("{\"name\":"+"\""+str(name)+"\""+",\"image\":"+"\""+img+"\""+"}")
+FilePath="graph.json"
+num=0
+Grphjson=""
+def tojson():
 
-        i+=1
+    global num
+    global Grphjson
+    while(1):
+        E=[]
+        enity = Enity.Enity(E)
+        enities = enity.getEnity()
+        print("enities大小",len(enities))
+        i = 1
+        j = 1
+        source = []
+        target = []
+        ID = []
+        for row in enities:
+            source.append(row[0])
+            ID.append(row[0])
+        #转变索引
+        for row in enities:
+            if (row[3] == None):
+                target.append(0)
+            else:
+                target.append(row[3])
 
-        if(i<=len(enities)):
-            f.write(",\n")
-        else:
-            f.write("\n")
-    f.write("],\n")
+        for x in range(len(ID)):
+            for y in range(len(target)):
+                if (ID[x] == source[y]):
+                    source[y] = x
+                if (ID[x] == target[y]):
+                    target[y] = x
+        #写入json文件
+        with open(FilePath, 'w', encoding="UTF-8") as f:
+            #nodes:
+            Grphjson += "{\n\"nodes\":\n[\n"
+            for row in enities:
+                img = "res/image/vmware.png"
+                #选择图片
+                if row[4] in imageSwitch:
+                    typeid = row[4]
+                    img = imageSwitch[typeid]
 
-#links:
-    f.write("\"links\":\n[\n")
-    for x in range(len(source)):
-            f.write("{\"source\":" + str(source[x]) + "," + "\"target\":" + str(target[x]) + "}")
-            j += 1
-            if (j <= len(source)):
-                f.write(",")
-            f.write("\n")
-    f.write("]\n}")
-    f.close()
+
+                name = row[1]
+                Grphjson+="{\"name\":" + "\"" + str(name) + "\"" + ",\"image\":" + "\"" + img + "\"" + "}"
+
+                i += 1
+                if (i <= len(enities)):
+                    Grphjson+=",\n"
+
+                else:
+                    Grphjson+="\n"
+
+            Grphjson += "],\n"
+
+
+            #links:
+            Grphjson+="\"links\":\n[\n"
+
+            for x in range(len(source)):
+                Grphjson+="{\"source\":" + str(source[x]) + "," + "\"target\":" + str(target[x]) + "}"
+
+                j += 1
+                if (j <= len(source)):
+                    Grphjson+=","
+
+                Grphjson+="\n"
+
+            Grphjson+="]\n}"
+
+            f.write(Grphjson)
+            f.close()
+
+            print("写入字节：",len(Grphjson))
+            Grphjson = ""
+        time.sleep(10)
+
+
+t = threading.Thread(target=tojson)
+t.start()
 
