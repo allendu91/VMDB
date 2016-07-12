a4596fd9bee05354708c444cdf4afecedc013ce4
Vanqingyu <vanqingyu@sina.com>
2016-07-12 10:58:36 +0800

    做出CPU的DATAFRAME

--- PGManage.py
+++ PGManage.py
@@ -7,8 +7,9 @@ class PG:
     #获得PostgreSql连接
     def getPGConnection(self):
         try:
-            self.conn = psycopg2.connect("user=postgres password=123456 dbname=VM")
+            self.conn = psycopg2.connect("user=postgres host=192.168.1.66 password=123456 dbname=VM")
             self.cursor = self.conn.cursor()
+            return self.conn
         except:
             print("Unexpected error:", sys.exc_info()[0])
             raise
--- VMDA/GetData.py
+++ VMDA/GetData.py
@@ -2,14 +2,11 @@
 import psycopg2
 import pandas as pd
 from pandas import DataFrame
-conn = psycopg2.connect("user=postgres host=192.168.1.112 password=123456 dbname=VM")
+conn = psycopg2.connect("user=postgres host=192.168.1.66 password=123456 dbname=VM")
 cursor=conn.cursor()
-cursor.execute("select stat_name,entity,stat_group,stat_rollup_type,stat_value from vm_state where entity like 'vm%' or entity like 'host%'")
+cursor.execute("select stat_name,stat_group,stat_rollup_type,stat_value from vm_state where entity like 'vm%' and stat_group like 'cpu'")
 rows=cursor.fetchall()
 conn.close()
-Frame=DataFrame(rows,columns=('State_Name','Entity_Name','State_Group','State_Type','State_Value'))
-df=Frame.groupby(['State_Name','Entity_Name','State_Group','State_Type'])
-frame=DataFrame(columns=['Group','Value'])
-for name, group in df:
-    for row in group['State_Value']:
-        frame.loc[len(frame)] = [name, row]
+Frame=DataFrame(rows,columns=('State_Name','State_Group','State_Type','State_Value'))
+Mydict={}
+CPU=DataFrame.from_dict(Mydict,orient='index').transpose()
